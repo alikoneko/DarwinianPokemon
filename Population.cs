d2bb8fbe078090a2fe1cef67adc15ecec98c87fc
Alison <alison.neer@gmail.com>
2015-09-21 22:09:52 -0400

    Ground work for population
    The genetic algorithm should care about fitness, and assign it.

--- Population.cs
+++ Population.cs
@@ -8,15 +8,15 @@ namespace DarwinianPokemon
 {
     class Population
     {
-        private Dictionary<Pokemon, float> pokemon_population;
+        private List<Pokemon> population;
 
         public Population(int hp_min, int hp_max, int attack_min, int attack_max, int defense_min, int defense_max, int type, int initial_population)
         {
-            Random rngesus = new Random();
-            List<Pokemon> pokemon = new List<Pokemon>();
+            Random rngesus = new Random(); //praise be to RNGesus, Random is random enough for this. A mersenne twister is just fine.
+            population = new List<Pokemon>();
             for (int index = 0; index < initial_population; index++)
             {
-                pokemon.Add(new Pokemon(type, rngesus.Next(hp_min, hp_max), rngesus.Next(attack_min, attack_max), rngesus.Next(defense_min, defense_max)));
+                population.Add(new Pokemon(type, rngesus.Next(hp_min, hp_max), rngesus.Next(attack_min, attack_max), rngesus.Next(defense_min, defense_max)));
             }
         }
     }
