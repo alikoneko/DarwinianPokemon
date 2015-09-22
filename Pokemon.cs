a7257b070ba85b2391ed90e6e1ad5101f932fadb
Alison <alison.neer@gmail.com>
2015-09-22 11:05:46 -0400

    Stat rework for population
    Decided to go with full poke-stats
    About to start Fight class. Fight will return a dictionary of pokemon
    with a fitness score assigned to each.

--- Pokemon.cs
+++ Pokemon.cs
@@ -10,46 +10,38 @@ namespace DarwinianPokemon
     class Pokemon
     {
         private int type_1;
-        //private int type_2;
+        private int type_2;
         private int hp;
         private int attack;
         private int defense;
-        //private int special_attack;
-        //private int special_defense;
-        //private int speed;
+        private int special_attack;
+        private int special_defense;
+        private int speed;
         //private string name;
 
-        public Pokemon(int type_1, int hp, int attack, int defense)
+        public Pokemon(int type_1, int type_2, int hp, int attack, int defense, int special_attack, int special_defense, int speed)
         {
             this.type_1 = type_1;
+            this.type_2 = type_2;
             this.hp = hp;
             this.attack = attack;
             this.defense = defense;
+            this.special_attack = special_attack;
+            this.special_defense = special_defense;
+            this.speed = speed;
             //this.name = name;
         }
-        //public Pokemon(int type_1, int type_2, int hp, int attack, int defense, int special_attack, int special_defense, int speed, string name)
-        //{
-        //    this.type_1 = type_1;
-        //    this.type_2 = type_2;
-        //    this.hp = hp;
-        //    this.attack = attack;
-        //    this.defense = defense;
-        //    this.special_attack = special_attack;
-        //    this.special_defense = special_defense;
-        //    this.speed = speed;
-        //    this.name = name;
-        //}
 
         public Pokemon(Pokemon pokemon)
         {
             this.type_1 = pokemon.type_1;
-            //this.type_2 = pokemon.type_2;
+            this.type_2 = pokemon.type_2;
             this.hp = pokemon.hp;
             this.attack = pokemon.attack;
             this.defense = pokemon.defense;
-            //this.special_attack = pokemon.special_attack;
-            //this.special_defense = pokemon.special_defense;
-            //this.speed = pokemon.speed;
+            this.special_attack = pokemon.special_attack;
+            this.special_defense = pokemon.special_defense;
+            this.speed = pokemon.speed;
             //this.name = pokemon.name;
         }
 
@@ -57,11 +49,14 @@ namespace DarwinianPokemon
         {
             string pokemon = "";
             //pokemon += name + "\n";
-            pokemon += type_1 + "\n";
-            pokemon += "Stats:\n";
-            pokemon += "hp: " + hp + "\n";
-            pokemon += "atk: " + attack + "\n";
-            pokemon += "defense: " + defense + "\n";
+            pokemon += type_1 + " " + type_2 + "\n"
+                + "Stats:\n"
+                + "hp: " + hp + "\n"
+                + "atk: " + attack + "\n"
+                + "defense: " + defense + "\n"
+                + "special attack: " + special_attack + "\n"
+                + "special defense: " + special_defense + "\n"
+                + "speed: " + speed + "\n";
             return pokemon;
         }
     }
--- Population.cs
+++ Population.cs
@@ -10,13 +10,20 @@ namespace DarwinianPokemon
     {
         private List<Pokemon> population;
 
-        public Population(int hp_min, int hp_max, int attack_min, int attack_max, int defense_min, int defense_max, int type, int initial_population)
+        /*
+         * This generates an initial population.  
+         */
+        public Population(int type_1, int type_2, int hp_min, int hp_max, 
+            int attack_min, int attack_max, int defense_min, int defense_max, 
+            int special_attack_min, int special_attack_max, int special_defense_min, int special_defense_max, 
+            int speed_min, int speed_max, int initial_population)
         {
             Random rngesus = new Random(); //praise be to RNGesus, Random is random enough for this. A mersenne twister is just fine.
             population = new List<Pokemon>();
             for (int index = 0; index < initial_population; index++)
             {
-                population.Add(new Pokemon(type, rngesus.Next(hp_min, hp_max), rngesus.Next(attack_min, attack_max), rngesus.Next(defense_min, defense_max)));
+                population.Add(new Pokemon(type_1, type_2, rngesus.Next(hp_min, hp_max), rngesus.Next(attack_min, attack_max), rngesus.Next(defense_min, defense_max), 
+                    rngesus.Next(special_attack_min, special_attack_max), rngesus.Next(special_defense_min, special_defense_max), rngesus.Next(speed_min, speed_max)));
             }
         }
     }
