07108e50766a97dcffea14d148fc54da9d5e12f5
Alison <alison.neer@gmail.com>
2015-09-24 18:42:13 -0400

    Gust
    Low damage special attack move

---
+++ Attacks/Gust.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarwinianPokemon.Attacks
+{
+    class Gust
+    {
+        Random random = new Random();
+        public Gust()
+        {
+            random = ServiceRegistry.GetInstance().GetRandom();
+        }
+
+        public int GetDamage(Pokemon attacker, Pokemon defender)
+        {
+            double damage = (2 * attacker.Level() + 10) / 250;
+            double modifier = random.NextDouble() * (1.00f - 0.85) + 0.85;
+            if (random.Next(0, 1) == 0) //determines critical hit!
+            {
+                modifier *= 2;
+            }
+
+            damage *= (attacker.GetSpecialAttack()) / (defender.GetSpecialDefense());
+            damage *= 40;
+            damage += 2;
+            damage *= modifier;
+
+            return (int)(Math.Round(damage));
+        }
+    }
+}
