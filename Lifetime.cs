48f9345bc9c722c292184d6c9f6a4a0d67a99a0d
Alison <alison.neer@gmail.com>
2015-09-23 20:04:12 -0400

    Lifetime added
    Lifetime of a specific set of genes

---
+++ Lifetime.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarwinianPokemon
+{
+    class Lifetime
+    {
+        private int kills;
+        private int deaths;
+        Pokemon pokemon;
+
+        public Lifetime(Pokemon pokemon)
+        {
+            this.pokemon = pokemon;
+            kills = 0;
+            deaths = 0;
+        }
+
+        public void Fight(Pokemon contestant)
+        {
+            Fight fight = new Fight(pokemon, contestant);
+            if (fight.Winner() == pokemon)
+            {
+                kills++;
+            }
+            else
+            {
+                deaths++;
+            }
+        }
+        public double GetKDR()
+        {
+            return (double) kills / (kills + deaths);
+        }
+
+    }
+}
