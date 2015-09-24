23c93c91e8b6f684df27fe75e78fef9c40d32b4e
Alison <alison.neer@gmail.com>
2015-09-24 10:16:30 -0400

    amend...
    I'm bad.

--- Generation.cs
+++ Generation.cs
@@ -8,12 +8,20 @@ namespace DarwinianPokemon
 {
     class Generation
     {
-        private List<Pokemon> population;
+        private List<Lifetime> population;
         private Random random = new Random();
 
         public Generation(List<Pokemon> population)
         {
-            this.population = population;
+            this.population = new List<Lifetime>();
+
+            foreach (Pokemon pokemon in population)
+            {
+                this.population.Add(new Lifetime(pokemon));
+            }
+
+            ShufflePopulation(this.population);
+
         }
 
         public List<Pokemon> Survivors()
@@ -23,37 +31,52 @@ namespace DarwinianPokemon
             population.OrderBy(f => random.Next());
 
 
-            foreach(Pokemon pokemon in population)
+            foreach (Lifetime lifetime in population)
             {
-                int fights_won = 0;
                 int fight_count = 5;
+                if (!lifetime.GetPokemon().Dead()) //if the pokemon is dead, move on. its dead.
+                {
+                    for (int i = 0; i < fight_count; i++)
+                    {
+                        try
+                        {
+                            Pokemon contestant = FindContestant(lifetime.GetPokemon());
+                            lifetime.Fight(contestant);
+                            lifetime.GetPokemon().Heal();
+                        }
+                        catch (OpponentNotFoundException e)
+                        {
 
-                for(int i = 0; i < fight_count; i++) {
-                    Pokemon contestant = FindContestant(pokemon);
-                    Fight fight = new Fight(pokemon, contestant);
+                        }
+                    }
                 }
+            }
+
+            foreach (Lifetime lifetime in population)
+            {
+
             }
 
             return survivors;
         }
 
-        private Pokemon FindContestant(Pokemon pokemon) {
+        private Pokemon FindContestant(Pokemon pokemon)
+        {
             Pokemon contestant;
 
             for (int i = 0; i < 3; i++)
             {
-                contestant = population[random.Next(population.Count)];
-                if (contestant != pokemon)
+                contestant = population[random.Next(population.Count)].GetPokemon();
+                if (contestant != pokemon && !contestant.Dead()) //can't fight a dead opponent.
                 {
                     return contestant;
                 }
             }
 
-            // throw an error if contestant is null here.
-            return null;
+            throw new OpponentNotFoundException();
         }
 
-        private void ShufflePopulation(List<Pokemon> population) 
+        private void ShufflePopulation(List<Lifetime> population) 
         {
             int count = population.Count;
             Random rngesus = new Random();
@@ -62,51 +85,11 @@ namespace DarwinianPokemon
             {
                 count--;
                 int k = rngesus.Next(count + 1);
-                Pokemon poke = population[k];
+                Lifetime temp = population[k];
                 population[k] = population[count];
-                population[count] = poke;
+                population[count] = temp;
             }
         }
 
-        private double average_stats(Pokemon pokemon)
-        {
-            double average = 0.0;
-
-            average += pokemon.GetHP();
-            average += pokemon.GetAttack();
-            average += pokemon.GetDefense();
-            average += pokemon.GetSpecialAttack();
-            average += pokemon.GetSpecialDefense();
-            average += pokemon.GetSpeed();
-
-            return average;
-        }
-
-        ////arbitrarily decides fitness
-        //private void fitness(Pokemon pokemon_a, Pokemon pokemon_b)
-        //{
-        //    if (fight.DetermineWinner(pokemon_a, pokemon_b, rngesus) == pokemon_a)
-        //    {
-        //        rated_population.Add(pokemon_a, average_stats(pokemon_a)/(average_stats(pokemon_a) + average_stats(pokemon_b)));
-        //        rated_population.Add(pokemon_b, -1 * (average_stats(pokemon_a)/(average_stats(pokemon_a) + average_stats(pokemon_b))));
-        //    }
-        //    else
-        //    {
-        //        rated_population.Add(pokemon_b, average_stats(pokemon_b) / (average_stats(pokemon_a) + average_stats(pokemon_b)));
-        //        rated_population.Add(pokemon_a, -1 * (average_stats(pokemon_a) / (average_stats(pokemon_a) + average_stats(pokemon_b))));
-        //    }
-        //}
-
-        //private void CullRandom()
-        //{
-        //    KeyValuePair<Pokemon, double> culled = rated_population.ElementAt(rngesus.Next(0, rated_population.Count));
-        //    rated_population.Remove(culled.Key);
-        //}
-
-        //private void CullBottom()
-        //{
-        //    rated_population.OrderBy(x => x.Value);
-
-        //}
     }
 }
