585e23bd6471931c44f38ba401b8fa52b64a0222
Alison <alison.neer@gmail.com>
2015-09-24 21:12:16 -0400

    Stat Range
    Helper for pokemon factory

---
+++ StatRange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarwinianPokemon
+{
+    class StatRange
+    {
+        int min;
+        int max;
+        Random r = new Random();
+
+        public StatRange(int min, int max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+        public int Random()
+        {
+            return r.Next(min, max);
+        }
+    }
+}
