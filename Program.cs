58b53324874bbc4e632ab3f866098a1935988eba
Alison <alison.neer@gmail.com>
2015-11-11 20:23:32 -0500

    Ending ALife sim for now, making a new project for AI project
    TODO: GUI, food resources, make this an interesting ant farm, dammit

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ namespace DarwinianPokemon
     {
         static void Main(string[] args)
         {
-            int initial_population = 10;
-            int max_population = 100;
+            int initial_population = 100;
+            int max_population = 1000;
             PokemonFactoryRepository pokemon_factory_repository = new PokemonFactoryRepository();
             PokemonPopulation population = new PokemonPopulation(pokemon_factory_repository.All(), initial_population, max_population);
             foreach (Pokemon pokemon in population.GetPopulation())
